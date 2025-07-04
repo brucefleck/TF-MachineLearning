0bb061acda5daa59e7859ecc1192dce83f9544fd
Bruce <brucematiasfleck@gmail.com>
2025-07-03 22:23:07 -0500

    created first preprocessing script to unite all the data into one file

---
+++ code/preprocessing.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+
+folder_path = './data'
+
+dataframes = []
+
+# recorre cada archivo en la carpeta original del dataset y junta todos los datos en un solo dataframe 
+for filename in os.listdir(folder_path):
+    if filename.endswith('.csv') and filename.startswith('coin_'):        
+        # Carga el CSV
+        df = pd.read_csv(os.path.join(folder_path, filename))
+        
+        # Convertimos la fecha a formato datetime
+        df['Date'] = pd.to_datetime(df['Date'])
+        
+        # Asegura el orden por fecha
+        df = df.sort_values(by='Date')
+        
+        # Agregamos el DataFrame a la lista
+        dataframes.append(df)
+
+# Concatena todos los DataFrames en uno solo
+full_df = pd.concat(dataframes, ignore_index=True)
+
+# Reordena las columnas
+cols = ['Name', 'Symbol', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Marketcap']
+full_df = full_df[cols]
+
+# Mostramos un resumen
+print("Dataset unificado con forma:", full_df.shape)
+print(full_df.head())
